e0989d35cc6086f388b1ef01d286b2b41022fe37
visualnnz <visualnnz@gmail.com>
2024-12-10 14:39:51 +0900

    Feat: 데이터베이스 로그인, 로그아웃 기능 추가
    1. Log in: User 이름 입력, Password 입력 후 데이터베이스에 로그인
    2. Log out: 데이터베이스와의 연결을 끊고 프로그램 종료

--- term_project.py
+++ term_project.py
@@ -1,3 +1,16 @@
+import getpass
+import mysql.connector
+
+# MySQL 연결
+def connect_to_database(username, pw):
+    return mysql.connector.connect(
+        host="192.168.40.3",
+        user=username,
+        password=pw,
+        database="term_project",
+        port=4567
+    )
+
 # 메인 함수
 def main():
     menu = [
@@ -16,27 +29,48 @@ def main():
         "12. Add a club",
         "13. Add a club manager",
         "14. Add a group-study",
-        "15. Update a student information",
-        "16. Update a club information",
-        "17. Update a club manager information",
-        "18. Update a group-study information",
-        "19. Delete a student information",
-        "20. Delete a club information",
-        "21. Delete a club manger information",
-        "22. Delete a group-study information"
+        "15. Update a student",
+        "16. Update a club",
+        "17. Update a club manager",
+        "18. Update a group-study",
+        "19. Delete a student",
+        "20. Delete a club",
+        "21. Delete a club manger",
+        "22. Delete a group-study"
     ]
 
-    while True:
-        i = 1
-        print("\n===================================%s===================================" % (menu[0]))
-        while i <= len(menu) - 1:
-            if (len(menu) >= i + 2):
-                print("%-40s %-40s" % (menu[i], menu[i + 1]))
-            else:
-                print("%-40s" % (menu[i]))
-            i = i + 2
-        print("============================================================================")
-        choice = input("Enter your choice: ")
+    connected = False
+    try:
+        while True:
+            i = 1
+            print("\n===================================%s===================================" % (menu[0]))
+            while i <= len(menu) - 1:
+                if (len(menu) >= i + 2):
+                    print("%-40s %-40s" % (menu[i], menu[i + 1]))
+                else:
+                    print("%-40s" % (menu[i]))
+                i = i + 2
+            print("============================================================================")
+            choice = input("Enter your choice: ")
+
+            if choice == "1":
+                if (connected == False):
+                    user = input("User: ")
+                    pw = getpass.getpass("Password: ")
+                    connection = connect_to_database(user, pw)
+                    print("Connected to the database.")
+                    connected = True
+                else:
+                    print("You already have been connected to database.")
+            elif choice == "2":
+                break
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    finally:
+        if connection:
+            connection.close()
+            print("Connection closed.\n")
+            connected = False
 
 if __name__ == "__main__":
     main()
