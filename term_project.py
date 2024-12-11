ca68777454c851b84be0f3404e06a54638c779e9
visualnnz <visualnnz@gmail.com>
2024-12-11 10:41:55 +0900

    Feat: 각 relation의 데이터 일괄조회 기능 추가
    SELECT * FROM <relation>

--- term_project.py
+++ term_project.py
@@ -15,7 +15,7 @@ def connectToDatabase(username, pw):
 def insertData(connection, relation):
     cursor = connection.cursor()
 
-    if relation == "student":
+    if relation == "STUDENT":
         query = "INSERT INTO STUDENT VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
         print("-------------------------------------------")
         sin = input("Sin: ")
@@ -30,7 +30,7 @@ def insertData(connection, relation):
         cursor.execute(query, (sin, phone, sname, sex, major, super_sin, cin, signup_date))
         connection.commit()
         print(f"Data inserted: {sin}, {phone}, {sname}, {sex}, {major}, {super_sin}, {cin}, {signup_date}")
-    elif relation == "club":
+    elif relation == "CLUB":
         query = "INSERT INTO CLUB VALUES (%s, %s, %s)"
         print("-------------------------------------------")
         cin = input("Cin: ")
@@ -40,7 +40,7 @@ def insertData(connection, relation):
         cursor.execute(query, (cin, cname, location))
         connection.commit()
         print(f"Data inserted: {cin}, {cname}, {location}")
-    elif relation == "club manager":
+    elif relation == "CLUB_MANAGER":
         query = "INSERT INTO CLUB_MANAGER VALUES (%s, %s, %s, %s, %s, %s, %s)"
         print("-------------------------------------------")
         cin = input("Cin: ")
@@ -54,7 +54,7 @@ def insertData(connection, relation):
         cursor.execute(query, (cin, president_sin, p_start_date, vicepresident_sin, vp_start_date, treasurer_sin, t_start_date))
         connection.commit()
         print(f"Data inserted: {cin}, {president_sin}, {p_start_date}, {vicepresident_sin}, {vp_start_date}, {treasurer_sin}, {t_start_date}")
-    elif relation == "group-study":
+    elif relation == "GROUP_STUDY":
         query = "INSERT INTO GROUP_STUDY VALUES (%s, %s, %s, %s, %s, %s)"
         query2 = "INSERT INTO PARTICIPATE_IN VALUES (%s, %s %s)"
         print("-------------------------------------------")
@@ -69,6 +69,46 @@ def insertData(connection, relation):
         cursor.execute(query2, (leader_sin, gin, l_start_date))
         connection.commit()
         print(f"Data inserted: {gin}, {gname}, {leader_sin}, {l_start_date}, {subject}, {cin}")
+
+# 모든 데이터 검색
+def searchAllData(connection, relation):
+    cursor = connection.cursor()
+    query = "SELECT * FROM %s" % (relation)
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    if relation == "STUDENT":
+        print("--------------------------------------------------------------------------------------------------------")
+        print("%s%6s %5s %7s %4s %3s %3s%s%s %7s %7s %5s %2s%1s%s%s%s" % 
+            ("|", "학번", "|", "전화번호", "|", "이름", "|", "성별", "|", "학과", "|", "멘토학번", "|", "동아리번호", "|", "동아리가입날짜", "|"))
+        print("--------------------------------------------------------------------------------------------------------")
+        for row in results:
+            print(row)
+        print("--------------------------------------------------------------------------------------------------------")
+    elif relation == "CLUB":
+        print("----------------------------")
+        print("%s%s%s%s%s%s%s" % ("|", "동아리번호", "|", "동아리이름", "|", "장소", "|"))
+        print("----------------------------")
+        for row in results:
+            print(row)
+        print("----------------------------")
+    elif relation == "CLUB_MANAGER":
+        print("----------------------------------------------------------------------------------------------------------------------")
+        print("%s%s%s%s%s%13s%9s %s %s%13s%7s %s %s %s%s" % 
+            ("|", "동아리번호", "|", "회장 학번", "|", "회장 시작날짜", "|", "부회장 학번", "|", "부회장 시작날짜", "|", "총무 학번", "|", "총무 시작날짜", "|"))
+        print("----------------------------------------------------------------------------------------------------------------------")
+        for row in results:
+            print(row)
+        print("----------------------------------------------------------------------------------------------------------------------")
+    elif relation == "GROUP_STUDY":
+        print("----------------------------------------------------------------------------------------------")
+        print("%s%s%s %7s%6s %6s%2s %12s%8s %5s%5s%s%s" %
+            ("|", "그룹번호", "|", "그룹이름", "|", "관리자 학번", "|", "관리자 시작날짜", "|", "주제", "|", "동아리번호", "|"))
+        print("----------------------------------------------------------------------------------------------")
+        for row in results:
+            print(row)
+        print("----------------------------------------------------------------------------------------------")
+
 
 # 메인 함수
 def main():
@@ -123,30 +163,54 @@ def main():
                     print("You already have been connected to database.")
             elif choice == "2":
                 break
+            elif choice == "3":
+                if connected == False:
+                    print("You need to log in the database.")
+                    continue
+                else:
+                    searchAllData(connection, "STUDENT")
+            elif choice == "4":
+                if connected == False:
+                    print("You need to log in the database.")
+                    continue
+                else:
+                    searchAllData(connection, "CLUB")
+            elif choice == "5":
+                if connected == False:
+                    print("You need to log in the database.")
+                    continue
+                else:
+                    searchAllData(connection, "CLUB_MANAGER")
+            elif choice == "6":
+                if connected == False:
+                    print("You need to log in the database.")
+                    continue
+                else:
+                    searchAllData(connection, "GROUP_STUDY")
             elif choice == "11":
                 if connected == False:
                     print("You need to log in the database.")
                     continue
                 else:
-                    insertData(connection, "student")
+                    insertData(connection, "STUDENT")
             elif choice == "12":
                 if connected == False:
                     print("You need to log in the database.")
                     continue
                 else:
-                    insertData(connection, "club")
+                    insertData(connection, "CLUB")
             elif choice == "13":
                 if connected == False:
                     print("You need to log in the database.")
                     continue
                 else:
-                    insertData(connection, "club manager")
+                    insertData(connection, "CLUB_MANAGER")
             elif choice == "14":
                 if connected == False:
                     print("You need to log in the database.")
                     continue
                 else:
-                    insertData(connection, "group-study")
+                    insertData(connection, "GROUP_STUDY")
     except mysql.connector.Error as err:
         print(f"Error: {err}")
     finally:
